95f5deb9a6b80c820df4e522a88b3d88b19a4876
Jeremy <Jeremy_Merle@outlook.com>
2025-08-08 15:22:04 +0800

    edit site metadata

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -17,8 +17,8 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Workouts Map',
-  siteUrl: 'https://ben29.xyz',
-  logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+  siteUrl: 'https://www.strava.com/athletes/108871715',
+  logo: 'https://raw.githubusercontent.com/Jeremy-Merle/workouts_page/refs/heads/master/assets/avatar.jpg',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
@@ -27,12 +27,8 @@ const data: ISiteMetadataResult = {
       url: `${getBasePath()}/summary`,
     },
     {
-      name: 'Blog',
-      url: 'https://ben29.xyz',
-    },
-    {
-      name: 'About',
-      url: 'https://github.com/ben-29/workouts_page/blob/master/README-CN.md',
+      name: 'Strava',
+      url: 'https://www.strava.com/athletes/108871715',
     },
   ],
 };
